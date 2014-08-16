280ee1b791a347b6b152d7fe673bda2632a48b19
Joe Sullivan <josulliv101@gmail.com>
2014-08-15 22:58:12 -0400

    Added Project Store/Actions

--- node_modules/statics/testdata/node_modules/react/addons.js
+++
@@ -1,4 +0,0 @@
-module.exports = require('./lib/ReactWithAddons');
-if ('production' !== process.env.NODE_ENV) {
-  module.exports = require('./ReactJSErrors').wrap(module.exports);
-}
--- node_modules/statics/testdata/node_modules/react/react.js
+++
@@ -1,4 +0,0 @@
-module.exports = require('./lib/React');
-if ('production' !== process.env.NODE_ENV) {
-  module.exports = require('./ReactJSErrors').wrap(module.exports);
-}
